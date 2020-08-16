2ff1c71b5acfe041926f99789c2bcbc6256ab7f3
Alejandro <alejandroacho@hotmail.com>
2020-08-16 13:43:21 +0200

    music player added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from "@ionic/storage";
 
 @Component({
   selector: 'app-home',
@@ -9,25 +11,51 @@ export class HomePage {
   stats = [{}];
   totalCases = 0;
   totalDeaths = 0;
-  TotalRecovered = 0;
+  totalRecovered = 0;
   newCases = 0;
   newDeaths = 0;
   newRecovered = 0;
-  constructor() {}
+  song;
+  newTime;
+
+  constructor(private router: Router, private storage: Storage) {}
+
   getStats() {
     return fetch("https://api.covid19api.com/summary").then(response=>response.json());
   }
+
   ionViewDidEnter(){
     this.getStats().then(newStats=>{
       this.stats = newStats["Global"];
 
       this.totalCases = newStats["Global"]["TotalConfirmed"];
       this.totalDeaths = newStats["Global"]["TotalDeaths"];
-      this.TotalRecovered = newStats["Global"]["TotalRecovered"];
+      this.totalRecovered = newStats["Global"]["TotalRecovered"];
 
       this.newCases = newStats["Global"]["NewConfirmed"];
       this.newDeaths = newStats["Global"]["NewDeaths"];
       this.newRecovered = newStats["Global"]["NewRecovered"];
     })
+  }
+
+  logOut(){
+    this.song.pause();
+    this.storage.set('isUserLogged', false);
+    this.router.navigateByUrl("/login");
+  }
+
+  play(){
+    this.song = new Audio("../../assets/sad-again.ogg")
+    this.song.addEventListener("timeupdate", ()=>{
+      this.newTime = (this.song.currentTime*(this.song.duration/10))/500;
+    });
+    this.song.play();
+  }
+
+  pause(){
+    this.song.pause();
+    this.song.addEventListener("timeupdate", ()=>{
+      this.newTime = 0;
+    });
   }
 }
