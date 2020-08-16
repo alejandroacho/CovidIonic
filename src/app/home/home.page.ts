1821d67c63b6f0da84b21b7e86487a8f853e047e
Alejandro <alejandroacho@hotmail.com>
2020-08-16 03:42:46 +0200

    Api consumida correctamente

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  stats = [{}];
+  totalCases = 0;
+  totalDeaths = 0;
+  TotalRecovered = 0;
+  newCases = 0;
+  newDeaths = 0;
+  newRecovered = 0;
   constructor() {}
+  getStats() {
+    return fetch("https://api.covid19api.com/summary").then(response=>response.json());
+  }
+  ionViewDidEnter(){
+    this.getStats().then(newStats=>{
+      this.stats = newStats["Global"];
 
+      this.totalCases = newStats["Global"]["TotalConfirmed"];
+      this.totalDeaths = newStats["Global"]["TotalDeaths"];
+      this.TotalRecovered = newStats["Global"]["TotalRecovered"];
+
+      this.newCases = newStats["Global"]["NewConfirmed"];
+      this.newDeaths = newStats["Global"]["NewDeaths"];
+      this.newRecovered = newStats["Global"]["NewRecovered"];
+    })
+  }
 }
